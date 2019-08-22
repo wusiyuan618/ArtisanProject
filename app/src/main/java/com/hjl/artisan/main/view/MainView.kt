4b3a20dea0d0011c947ea481593200f494752635
Administrator <719126555@qq.com>
2019-08-22 12:02:24 +0800

    实测实量完成

--- app/src/main/java/com/hjl/artisan/main/view/MainView.kt
+++ app/src/main/java/com/hjl/artisan/main/view/MainView.kt
@@ -6,8 +6,14 @@ import com.hjl.artisan.R
 import com.hjl.artisan.main.presenter.MainPresenter
 import com.wusy.wusylibrary.base.BaseMVPActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import java.util.*
+
 
 class MainView : BaseMVPActivity<IMainView, MainPresenter>(), IMainView {
+    private var isExit = false
+    private var timerTask: TimerTask? = null
+    private var timer: Timer? = null
     override fun createPresenter(): MainPresenter? {
         return MainPresenter()
     }
@@ -27,11 +33,32 @@ class MainView : BaseMVPActivity<IMainView, MainPresenter>(), IMainView {
             supportFragmentManager,
             R.id.fragmentView
         )
+        timer = Timer()//用于退出键监听的二次点击退出功能
+
     }
 
     override fun getmContext(): Context? {
         return this
     }
-
+    /**
+     * 退出事件监听
+     */
+    override fun onBackPressed() {
+        if (isExit) {
+            val home = Intent(Intent.ACTION_MAIN)
+            home.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            home.addCategory(Intent.CATEGORY_HOME)
+            startActivity(home)
+        } else {
+            isExit = true
+            showToast("再按一次进入后台运行")
+            timerTask = object : TimerTask() {
+                override fun run() {
+                    isExit = false
+                }
+            }
+            timer!!.schedule(timerTask, 2000)
+        }
+    }
 
 }
